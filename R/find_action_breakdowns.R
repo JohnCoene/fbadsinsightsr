a2bc4153e52780a3586b67699cb81020359b7687
CMAnalytics <john.coene@conew.com>
2015-12-18 14:54:52 +0800

    init

---
+++ R/find_action_breakdowns.R
@@ -0,0 +1,13 @@
+#' find_action_breakdowns
+#' 
+#' @description Returns all valid \code{action_breakdowns} which can be used in the get-family functions. No input, see usage.
+#' 
+#' @seealso \code{\link{get_account}}
+#' 
+#' @export
+#' 
+#' @author John Coene <john.coene@@cmcmc.com>
+find_action_breakdowns <- function() {
+  action_breakdowns <- c("action_carousel_card_id", "action_carousel_card_name", "action_destination", "action_device", "action_target_id", "action_type", "action_video_type")
+  return(action_breakdowns)
+}
