074d7a414356d576d27717c7fccefa7cb0a657ef
CMAnalytics <john.coene@conew.com>
2015-12-22 15:27:40 +0800

    example added

--- R/findActionBreakdowns.R
+++ R/findActionBreakdowns.R
@@ -2,7 +2,23 @@
 #' 
 #' @description Returns all valid \code{action_breakdowns} which can be used in the get-family functions. No input, see usage.
 #' 
-#' @seealso \code{\link{getAny}}
+#' @examples 
+#' \dontrun{
+#' # get information on account
+#' info <- findInfo(account.id = "act_123456789012345", token = "XXXXXXXXXXX")
+#' 
+#' # take random ad.id
+#' set.seed(123)
+#' rand_id <- sample(info$ads$id, 1)
+#' 
+#' # carousel-related action_breakdowns
+#' carousel <- findActionBreakdowns()[grep("carousel", findActionBreakdowns())]
+#' 
+#' # fetch Ad data broken down by actions related to carousel
+#' data <- getAny(id = rand_id, token = "XXXXXXXXXXX", action.breakdowns = carousel)
+#' }
+#' 
+#' @seealso \code{\link{getAny}}, \code{\link{getInfo}}
 #' 
 #' @export
 #' 
