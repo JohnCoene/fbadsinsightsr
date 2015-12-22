9a0f2980e2639ed3ca9ba2385fd801de4a8cae20
CMAnalytics <john.coene@conew.com>
2015-12-22 19:40:45 +0800

    init

---
+++ R/findAccounts.R
@@ -0,0 +1,78 @@
+#' findAccounts
+#' 
+#' @description Fetches all accounts under one business ID. All accounts to which the user has access to that is.
+#' 
+#' @param id Your business.facebook.com ID or your user id.
+#' @param token A valid token as returned by \code{\link{fbAuthenticate}} or a short-term token from \href{https://developers.facebook.com/tools/explorer}{facebook Graph API Explorer}.
+#' @param n Number of results to retrieve, defaults to \code{100}. When you make an API request, you will usually not receive all of the results of that request in a single response. This is because some responses could contain thousands of objects so most responses are paginated by default. \code{previous} fetches the previous page of response (after the initial query) similarly \code{next} fetches the next page and \code{NULL} does not paginate (only makes one query).
+#' 
+#' @details Returns data.frame of account IDs.
+#' 
+#' @examples 
+#' \dontrun{
+#' # get account ids
+#' act <- findAccounts(id = "me", token = "XXXXXXXXXXX")
+#' 
+#' # get information on account
+#' info <- findInfo(account.id = act[2,2], token = "XXXXXXXXXXX")
+#' }
+#' 
+#' @seealso \code{\link{findInfo}}
+#' 
+#' @author John Coene <john.coene@@cmcm.com>
+#' 
+#' @export
+findAccounts <- function(id, token, n = 100) {
+  
+  # check inputs
+  if(missing(id)){
+    stop("Missing account.id")
+  } else if (missing(token)){
+    stop("Missing token")
+  }
+  
+  # check token
+  token <- checkToken(token)
+  
+  url <- paste0("https://graph.facebook.com/v2.5/", id, "/adaccounts?fields=",
+                "&access_token=", token)
+  
+  # call api
+  response <- httr::GET(url)
+  
+  # parse to list
+  json <- rjson::fromJSON(rawToChar(response$content))
+  
+  # check if query successful 
+  if(length(json$error$message)){
+    stop(paste("this is likely due to account.id or token. Error Message returned: ",
+               json$error$message))
+  }
+  
+  # parse
+  data <- parseJSON(json)
+  
+  i <- 1
+  
+  # Paginate
+  while (nrow(data) < n & 
+         !is.null(json$paging$`next`)) {
+    # GET
+    response <- httr::GET(json$paging$`next`)
+    
+    # parse
+    json <- rjson::fromJSON(rawToChar(response$content))
+    
+    # bind
+    data <- plyr::rbind.fill(data, parseJSON(json))
+    
+    # pause between queries if more than 2 to avoid lengthy calls
+    i <- i + 1
+    
+    if(i >= 3) {
+      Sys.sleep(2)
+    }
+  }
+  
+  return(data)
+}
