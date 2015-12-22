15e842ae032e4aef550949b8813f3dbbf9cb511c
CMAnalytics <john.coene@conew.com>
2015-12-22 14:28:57 +0800

    paginate change to n

--- R/getAny.R
+++ R/getAny.R
@@ -12,7 +12,7 @@
 #' @param level Represents the level of result (Optional). Must be one  of \code{ad}, \code{adset}, \code{campaign}, \code{account}.
 #' @param time.increment If it is an integer, it is the number of days from 1 to 90. After you pick a reporting period by using \code{time.range} or \code{date.preset}, you may choose to have the results for the whole period, or have results for smaller time slices. If "all_days" is used, it means one result set for the whole period. If "monthly" is used, you will get one result set for each calendar month in the given period. Or you can have one result set for each N-day period specified by this param.
 #' @param time.range time range must be \code{c(since = 'YYYY-MM-DD', until='YYYY-MM-DD')}
-#' @param paginate Defaults to \code{NULL}. When you make an API request, you will usually not receive all of the results of that request in a single response. This is because some responses could contain thousands of objects so most responses are paginated by default. \code{previous} fetches the previous page of response (after the initial query) similarly \code{next} fetches the next page and \code{NULL} does not paginate (only makes one query).
+#' @param n Number of results to retrieve, defaults to \code{100}. When you make an API request, you will usually not receive all of the results of that request in a single response. This is because some responses could contain thousands of objects so most responses are paginated by default. \code{previous} fetches the previous page of response (after the initial query) similarly \code{next} fetches the next page and \code{NULL} does not paginate (only makes one query).
 #' @param token A valid token as returned by \code{\link{fbAuthenticate}} or a short-term token from \href{https://developers.facebook.com/tools/explorer}{facebook Graph API Explorer}.
 #' 
 #' @details This function refers to the following API call \url{https://developers.facebook.com/docs/marketing-api/reference/ad-account/insights/},
@@ -40,7 +40,7 @@ getAny <- function(id, fields = "default",
                      action.breakdowns = NULL, action.report.time = NULL,
                      breakdowns = NULL, date.preset = NULL, level = NULL, 
                      time.increment = NULL, time.range = NULL, 
-                     paginate = NULL, token) {
+                     n = 100, token) {
   
   # check inputs
   if(missing(id)){
@@ -195,31 +195,26 @@ getAny <- function(id, fields = "default",
   # parse
   data <- parseJSON(json)
   
+  i <- 1
+  
   # Paginate
-  if(length(paginate)) {
+  while (nrow(data) < n & 
+         !is.null(json$paging$`next`)) {
+    # GET
+    response <- httr::GET(json$paging$`next`)
     
-    # test if value is correct
-    if(paginate != "next" && paginate != "previous" && !is.null(paginate)){
-      stop("paginate can be either 'next', 'previous' or NULL")
-      # double check and fetch URL
-    } else if (paginate == "next" || paginate == "previous" ) {
-      if(paginate == "next" && length(json$paging$`next`)){
-        url <- json$paging$`next`
-      } else {
-        url <- json$paging$previous
-      }
-      
-      # if inpt corrects and url present then loop
-      while (length(url)) {
-        response <- httr::GET(url)
-        json <- rjson::fromJSON(rawToChar(response$content))
-        data <- plyr::rbind.fill(data, parseJSON(json))
-      }
+    # parse
+    json <- rjson::fromJSON(rawToChar(response$content))
+    
+    # bind
+    data <- plyr::rbind.fill(data, parseJSON(json))
+    
+    # pause between queries if more than 2 to avoid lengthy calls
+    i <- i + 1
+    
+    if(i >= 3) {
+      Sys.sleep(2)
     }
-    
-    # NULL
-  } else if(is.null(paginate)){
-    # do nothing
   }
   
   return(data)
