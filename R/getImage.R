e0fddad524e0f176a239f89f9e8c1f603067dd96
CMAnalytics <john.coene@conew.com>
2016-01-07 16:18:26 +0800

    account.id check added

--- R/getImage.R
+++ R/getImage.R
@@ -48,40 +48,8 @@ getImage <- function(account.id, token, n = 100, verbose = FALSE){
     stop("Missing id")
   } else if (missing(token)){
     stop("Missing token")
-  } 
-  
-  # make date.preset NULL if time.range specified
-  if (length(date.preset) == 1 && length(time.range) >= 1) {
-    date.preset <- NULL
-    warning("date.preset is ignored as time.range is specified")
-  }
-  
-  if (length(date.preset) == 1) {
-    
-    # test
-    testParam("date_preset", date.preset)
-    
-    date.preset <- paste0("&date_preset=", date.preset)
-  } else if (length(date.preset) > 1) {
-    stop("date.preset can only hold one of the following values: today, yesterday, last_3_days, this_week, last_week, last_7_days, last_14_days, last_28_days, last_30_days, last_90_days, this_month, last_month, this_quarter, last_3_months, lifetime")
-  }
-  
-  # time range
-  if(length(time.range) == 2) {
-    
-    date_check <- as.Date(time.range[2], format= "%Y-%m-%d")
-    
-    # further checks
-    if(names(time.range)[1] != "since"){
-      stop("time.range must be - c(since = 'YYYY-MM-DD', until='YYYY-MM-DD')")
-    } else if (class(date_check) == "try-error" || is.na(date_check)){
-      stop("Wrong date format. Must be YYYY-MM-DD")
-    }
-    
-    time.range <- paste0("&since=", time.range[1], "&", "until=", time.range[2])
-    
-  } else if (length(time.range) > 2) {
-    stop("time.range must be - c(since = 'YYYY-MM-DD', until='YYYY-MM-DD')")
+  } else if (length(account.id[grep("act_", account.id)]) == 0){
+    stop("Must be account id. starting with act_")
   }
   
   # check token verison
