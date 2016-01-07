ed19dc1c03b857cae7c9276f58aa83b3dc2109bf
CMAnalytics <john.coene@conew.com>
2016-01-07 19:05:06 +0800

    added getCreative

--- R/getImage.R
+++ R/getImage.R
@@ -4,6 +4,8 @@
 #' 
 #' @param account.id Your ad account id, starting by "act_" and followed by 15 digits (Required), see \href{https://www.facebook.com/business/help/1492627900875762}{how to find yours}.
 #' @param token A valid token as returned by \code{\link{fbAuthenticate}} or a short-term token from \href{https://developers.facebook.com/tools/explorer}{facebook Graph API Explorer}.
+#' @param date.preset Represents a relative time range (Optional). This field is ignored if \code{time.range} is specified. Run \code{\link{findDatePreset}} to see all valid presets.
+#' @param time.range time range must be \code{c(since = 'YYYY-MM-DD', until='YYYY-MM-DD')}
 #' @param n Number of results to retrieve, defaults to \code{100}. When you make an API request, you will usually not receive all of the results of that request in a single response. This is because some responses could contain thousands of objects so most responses are paginated by default. \code{previous} fetches the previous page of response (after the initial query) similarly \code{next} fetches the next page and \code{NULL} does not paginate (only makes one query).
 #' @param verbose Defaults to \code{FALSE} if \code{TRUE} will print information on the query in the console.
 #' 
@@ -41,7 +43,8 @@
 #' @seealso \code{\link{fbAuthenticate}}, \code{\link{findAccounts}}, \code{\link{findObjects}}
 #'
 #' @export
-getImage <- function(account.id, token, n = 100, verbose = FALSE){
+getImage <- function(account.id, token, date.preset = NULL,
+                     time.range = NULL, n = 100, verbose = FALSE){
   
   # check inputs
   if(missing(account.id)){
@@ -50,6 +53,40 @@ getImage <- function(account.id, token, n = 100, verbose = FALSE){
     stop("Missing token")
   } else if (length(account.id[grep("act_", account.id)]) == 0){
     stop("Must be account id. starting with act_")
+  }
+  
+  # make date.preset NULL if time.range specified
+  if (length(date.preset) == 1 && length(time.range) >= 1) {
+    date.preset <- NULL
+    warning("date.preset is ignored as time.range is specified")
+  }
+  
+  if (length(date.preset) == 1) {
+    
+    # test
+    testParam("date_preset", date.preset)
+    
+    date.preset <- paste0("&date_preset=", date.preset)
+  } else if (length(date.preset) > 1) {
+    stop("date.preset can only hold one of the following values: today, yesterday, last_3_days, this_week, last_week, last_7_days, last_14_days, last_28_days, last_30_days, last_90_days, this_month, last_month, this_quarter, last_3_months, lifetime")
+  }
+
+  # time range
+  if(length(time.range) == 2) {
+    
+    date_check <- as.Date(time.range[2], format= "%Y-%m-%d")
+    
+    # further checks
+    if(names(time.range)[1] != "since"){
+      stop("time.range must be - c(since = 'YYYY-MM-DD', until='YYYY-MM-DD')")
+    } else if (class(date_check) == "try-error" || is.na(date_check)){
+      stop("Wrong date format. Must be YYYY-MM-DD")
+    }
+    
+    time.range <- paste0("&since=", time.range[1], "&", "until=", time.range[2])
+    
+  } else if (length(time.range) > 2) {
+    stop("time.range must be - c(since = 'YYYY-MM-DD', until='YYYY-MM-DD')")
   }
   
   # check token verison
