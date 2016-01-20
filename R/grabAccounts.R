c9b74ac9fb43196e5d413aa07ebec326bf4c6b79
CMAnalytics <john.coene@conew.com>
2016-01-20 14:55:50 +0800

    example fixed

--- R/grabAccounts.R
+++ R/grabAccounts.R
@@ -6,6 +6,7 @@
 #' @param id Your business.facebook.com ID or your user id.
 #' @param token A valid token as returned by \code{\link{fbAuthenticate}} or a short-term token from \href{https://developers.facebook.com/tools/explorer}{facebook Graph API Explorer}.
 #' @param n Number of results to retrieve, defaults to \code{100}. When you make an API request, you will usually not receive all of the results of that request in a single response. This is because some responses could contain thousands of objects so most responses are paginated by default. \code{previous} fetches the previous page of response (after the initial query) similarly \code{next} fetches the next page and \code{NULL} does not paginate (only makes one query).
+#' @param fields Defaults to \code{default} which returns name and id of accounts. See \code{\link{findFields}} to find all available fields.
 #' @param verbose Defaults to \code{FALSE} if \code{TRUE} will print information on the query in the console.
 #'
 #' @details Returns data.frame of account IDs and status. requires \code{ads_management} permission!
@@ -13,10 +14,10 @@
 #' @examples 
 #' \dontrun{
 #' # get account ids
-#' act <- findAccounts(id = "me", token = "XXXXXXXXXXX")
+#' act <- grabAccounts(id = "me", token = "XXXXXXXXXXX")
 #' 
 #' # get information on account
-#' data <- getAccount(account.id = act[2,2], token = "XXXXXXXXXXX")
+#' data <- getAccount(account.id = sample(act, 1), token = "XXXXXXXXXXX")
 #' }
 #' 
 #' @seealso \code{\link{getAccount}}
@@ -24,8 +25,8 @@
 #' @author John Coene <john.coene@@cmcm.com>
 #' 
 #' @export
-grabAccounts <- function(id, token, n = 100, verbose = FALSE,
-                         fields = "default") {
+grabAccounts <- function(id, token, n = 100, fields = "default",
+                         verbose = FALSE) {
   
   # check inputs
   if(missing(id)){
