5be847150085278662ce4dbbf08c6d94240afc4c
jcoenep <jcoenep@hotmail.com>
2016-04-04 11:48:24 +0800

    example

--- R/grabAccounts.R
+++ R/grabAccounts.R
@@ -8,16 +8,14 @@
 #' @param n Number of results to retrieve, defaults to \code{100}. When you make an API request, you will usually not receive all of the results of that request in a single response. This is because some responses could contain thousands of objects so most responses are paginated by default. \code{previous} fetches the previous page of response (after the initial query) similarly \code{next} fetches the next page and \code{NULL} does not paginate (only makes one query).
 #' @param fields Defaults to \code{default} which returns name and id of accounts. See \code{\link{findFields}} to find all available fields.
 #' @param verbose Defaults to \code{FALSE} if \code{TRUE} will print information on the query in the console.
+#' @param limit Number of results requested at each API call, defaults to 100.
 #'
 #' @details Returns data.frame of account IDs and status. requires \code{ads_management} permission!
 #' 
 #' @examples 
 #' \dontrun{
 #' # get account ids
-#' act <- grabAccounts(id = "me", token = "XXXXXXXXXXX")
-#' 
-#' # get information on account
-#' data <- getAccount(account.id = sample(act, 1), token = "XXXXXXXXXXX")
+#' accs <- grabAccounts(id = "act_123456789012345", token = "XXXXXX")
 #' }
 #' 
 #' @seealso \code{\link{getAccount}}
@@ -26,7 +24,7 @@
 #' 
 #' @export
 grabAccounts <- function(id, token, n = 100, fields = "default",
-                         verbose = FALSE) {
+                         verbose = FALSE, limit = 100) {
   
   # check inputs
   if(missing(id)){
@@ -38,7 +36,7 @@ grabAccounts <- function(id, token, n = 100, fields = "default",
   
   fb_data <- findObjects(id = id, token = token, fields = fields, 
                          n = n, verbose = verbose, object = "adaccounts",
-                         FUN = "grabAccounts")
+                         FUN = "grabAccounts", limit = limit)
   
   if (nrow(fb_data) == 0) warning(paste("No data."), call. = FALSE)
   
