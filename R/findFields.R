3d825631597b1d93248437d38b321922bbb135ca
John Coene <jcoenep@gmail.com>
2016-07-08 10:01:16 +0800

    getLabCreatives link in docs fixed

--- R/findFields.R
+++ R/findFields.R
@@ -28,6 +28,7 @@
 #' \item \code{\link{getLabCampaigns}}
 #' \item \code{\link{getLabAdsets}}
 #' \item \code{\link{getLabAds}}
+#' \item \code{\link{getLabCreatives}}
 #' }
 #' 
 #' @examples 
@@ -56,37 +57,42 @@ findFields <- function(fct ="getAny") {
      fct == "getAdset" || fct == "getAd") {
     
     #list
-    fields <- c("date_start", "date_stop", "account_id", "account_name", "ad_id",
-                "ad_name", "buying_type", "campaign_id", "campaign_name", 
-                "adset_id", "adset_name", "action_carousel_card_id", 
-                "action_carousel_card_name", "actions", "unique_actions",
-                "total_actions", "total_unique_actions", "action_values", 
-                "total_action_value", "impressions", "social_impressions",
-                "social_clicks", "unique_impressions", 
-                "unique_social_impressions", "clicks",
-                "unique_clicks", "unique_social_clicks", "spend", "frequency", 
-                "social_spend", "deeplink_clicks", "app_store_clicks", 
-                "website_clicks", "cost_per_inline_post_engagement", 
-                "inline_link_clicks", "cost_per_inline_link_click", 
-                "inline_post_engagement", "call_to_action_clicks", 
+    fields <- c("date_start", "date_stop", "account_id", "account_name", 
+                "ad_id", "ad_name", "buying_type", "campaign_id", 
+                "campaign_name", "adset_id", "adset_name", "objective", 
+                "actions", "unique_actions", "total_actions", 
+                "total_unique_actions", "action_values", 
+                "total_action_value", "impressions", "social_impressions", 
+                "clicks", "social_clicks", "unique_impressions", 
+                "unique_social_impressions", "unique_clicks", 
+                "unique_social_clicks", "spend", "frequency", "social_spend", 
+                "deeplink_clicks", "app_store_clicks", "website_clicks", 
+                "cost_per_inline_post_engagement", "inline_link_clicks", 
+                "cost_per_inline_link_click", "inline_post_engagement", 
+                "unique_inline_link_clicks", 
+                "cost_per_unique_inline_link_click", "inline_link_click_ctr", 
+                "unique_inline_link_click_ctr", "call_to_action_clicks", 
                 "newsfeed_avg_position", "newsfeed_impressions", 
-                "newsfeed_clicks", "reach", "social_reach", "ctr",
-                "unique_ctr", "unique_link_clicks_ctr", "cpm", "cpp",
+                "newsfeed_clicks", "reach", "social_reach", "ctr", "unique_ctr",
+                "unique_link_clicks_ctr", "cpc", "cpm", "cpp", 
                 "cost_per_total_action", "cost_per_action_type", 
                 "cost_per_unique_click", "cost_per_10_sec_video_view", 
-                "cost_per_unique_action_type", "relevance_score", "website_ctr",
-                "video_avg_sec_watched_actions", "video_avg_pct_watched_actions",
-                "video_p25_watched_actions", "video_p50_watched_actions", 
-                "video_p75_watched_actions", "video_p95_watched_actions", 
-                "video_p100_watched_actions", "video_complete_watched_actions",
-                "video_10_sec_watched_actions", "video_15_sec_watched_actions",
-                "video_30_sec_watched_actions", "estimated_ad_recallers",
-                "estimated_ad_recallers_lower_bound", 
-                "estimated_ad_recallers_upper_bound", 
-                "estimated_ad_recall_rate", 
-                "estimated_ad_recall_rate_lower_bound", 
+                "cost_per_unique_action_type", "relevance_score", "website_ctr", 
+                "video_avg_sec_watched_actions", 
+                "video_avg_pct_watched_actions", "video_p25_watched_actions", 
+                "video_p50_watched_actions", "video_p75_watched_actions", 
+                "video_p95_watched_actions", "video_p100_watched_actions", 
+                "video_complete_watched_actions", "video_10_sec_watched_actions", 
+                "video_15_sec_watched_actions", "video_30_sec_watched_actions",
+                "estimated_ad_recallers", "estimated_ad_recallers_lower_bound",
+                "estimated_ad_recallers_upper_bound", "estimated_ad_recall_rate", 
+                "estimated_ad_recall_rate_lower_bound",
                 "estimated_ad_recall_rate_upper_bound", 
-                "cost_per_estimated_ad_recallers", "place_page_name")
+                "cost_per_estimated_ad_recallers", "canvas_avg_view_time", 
+                "canvas_avg_view_percent", "place_page_name", "ad_bid_type", 
+                "ad_bid_value", "ad_delivery", "adset_bid_type", 
+                "adset_bid_value", "adset_budget_type", "adset_budget_value", 
+                "adset_delivery", "adset_start", "adset_end")
     
   } else if(fct == "grabCampaigns" || fct == "grabAdsets" || 
             fct == "grabAds"){
@@ -166,7 +172,7 @@ findFields <- function(fct ="getAny") {
                 "full_picture", "icon", "is_published", "id")
     
   } else if(fct == "getLabCampaigns" || fct == "getLabAdsets" || 
-            fct == "getLabAds") {
+            fct == "getLabAds" || fct == "getLabCreatives") {
     
     fields <- c("account_id", "adlabels", "buying_type", "configured_status", 
                 "created_time", "effective_status", "id", "name", "objective",
