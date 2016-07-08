2cbf87d5be343aa371e08148b3dd695b75d56ff0
John Coene <jcoenep@gmail.com>
2016-07-08 17:33:36 +0800

    startup message changed

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 # startUpMessage -------------------------------
 .onAttach <- function(libname, pkgname = "fbAdsInsightsR") {
-  packageStartupMessage("Welcome to Cheetah Mobile fbAdsInsightsR package ", 
-                        "\n",
-                        "Run ?fbAdsInsightsR to get started")
+  packageStartupMessage("Run ?fbAdsInsightsR to get started\n",
+                        "by Cheetah Mobile")
 }
